dfa5487f4f97dee0fad2f5a0523108c6d7e9a389
jagathda <mail.jagath@gmail.com>
2024-10-09 20:09:50 +0300

    Initial commit

---
+++ test/event_bridge_lambda_fargate_cloud_watch_postgre_sql-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventBridgeLambdaFargateCloudWatchPostgreSqlCdk from '../lib/event_bridge_lambda_fargate_cloud_watch_postgre_sql-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event_bridge_lambda_fargate_cloud_watch_postgre_sql-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventBridgeLambdaFargateCloudWatchPostgreSqlCdk.EventBridgeLambdaFargateCloudWatchPostgreSqlCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
